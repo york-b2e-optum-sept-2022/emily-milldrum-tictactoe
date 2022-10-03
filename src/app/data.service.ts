1d2625e8e179979252973602c677a636b7d3bd3d
Emily <emilldr>
2022-10-03 16:58:44 -0500

    init

--- src/app/data.service.ts
+++ src/app/data.service.ts
@@ -53,11 +53,58 @@ export class DataService {
     }
     if (this.takeTurns){
       this.boxes[index].value = "x";
+      this.checkWin();
       this.takeTurns = false;
     }
     else{
       this.boxes[index].value = "o";
+      this.checkWin();
       this.takeTurns = true;
+    }
+  }
+
+  // possibly cleaner logic?
+  // const winCombos = [
+  //   [0, 1, 2],
+  //   [3, 4, 5],
+  //   [6, 7, 8],
+  //   [0, 4, 8],
+  //   [2, 4, 6],
+  //   [0, 3, 6],
+  //   [1, 4, 7],
+  //   [2, 5, 8]
+  // ]
+  victory: boolean = false;
+  checkWin() {
+    if (this.boxes[0].value !== " - "){
+      if (this.boxes[0].value === this.boxes[1].value && this.boxes[1].value === this.boxes[2].value) {
+        console.log(
+          this.boxes[0].value + this.boxes[1].value + this.boxes[2].value + ' win - row 1')
+      }
+      if (this.boxes[0].value === this.boxes[4].value && this.boxes[0].value === this.boxes[8].value) {
+        console.log(
+          this.boxes[0].value + this.boxes[4].value + this.boxes[8].value + ' win - diag')
+      }
+      if (this.boxes[0].value === this.boxes[3].value && this.boxes[0].value === this.boxes[6].value) {
+        console.log(
+          this.boxes[0].value + this.boxes[3].value + this.boxes[6].value + ' win - column 1')
+      }
+    }
+    if (this.boxes[3].value !== " - ") {
+      if (this.boxes[3].value === this.boxes[4].value && this.boxes[3].value === this.boxes[5].value) {
+        console.log(
+          this.boxes[3].value + this.boxes[4].value + this.boxes[5].value + ' win - row 2')
+      }
+    }
+    if (this.boxes[6].value !== " - ") {
+      if (this.boxes[6].value === this.boxes[7].value && this.boxes[6].value === this.boxes[8].value) {
+        console.log(
+          this.boxes[6].value + this.boxes[7].value + this.boxes[8].value + ' win - row 3')
+      }
+      if (this.boxes[2].value === this.boxes[4].value && this.boxes[2].value === this.boxes[6].value) {
+        console.log(
+          this.boxes[2].value + this.boxes[4].value + this.boxes[6].value + ' win - diag')
+      }
     }
   }
 }
